dcfb142c149fbe4ade0e68a05fc0ac6d101e9157
stefaan.ternier <stefaan.ternier@gmail.com>
2012-04-05 13:22:43 +0200

    gwt additions

---
+++ Appengine/src/org/celstec/arlearn2/gwt/client/Authoring.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright 2011 Google Inc. All Rights Reserved.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package org.celstec.arlearn2.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.smartgwt.client.types.Side;
+import com.smartgwt.client.widgets.layout.VLayout;
+import com.smartgwt.client.widgets.tab.Tab;
+import com.smartgwt.client.widgets.tab.TabSet;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Authoring implements EntryPoint {
+	private Button clickMeButton;
+	public void onModuleLoad() {
+		RootPanel rootPanel = RootPanel.get("container");
+
+		final TabSet topTabSet = new TabSet();
+		topTabSet.setSize("100%", "100%");
+		topTabSet.setTabBarPosition(Side.TOP);
+		topTabSet.setTabBarAlign(Side.LEFT);
+		
+		Tab tTab1 = new Tab("Games");  
+		  
+        tTab1.setCanClose(false);  
+  
+        
+        topTabSet.addTab(tTab1);  
+
+		
+		VLayout vLayout = new VLayout();  
+        vLayout.setMembersMargin(15);  
+        vLayout.addMember(topTabSet);  
+        vLayout.setSize("640", "480");
+        
+        rootPanel.add(vLayout);
+		
+	}
+}
