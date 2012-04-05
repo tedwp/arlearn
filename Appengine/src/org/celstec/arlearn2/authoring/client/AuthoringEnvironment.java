883fcf423573225931765f64dbe956f12cff190d
stefaan.ternier <stefaan.ternier@gmail.com>
2012-04-05 11:29:30 +0200

    initial adding files for smart gwt authoring env

---
+++ Appengine/src/org/celstec/arlearn2/authoring/client/AuthoringEnvironment.java
@@ -0,0 +1,12 @@
+package org.celstec.arlearn2.authoring.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class AuthoringEnvironment implements EntryPoint {
+
+	public void onModuleLoad() {
+		RootPanel rootPanel = RootPanel.get();
+	}
+
+}
