460c75f6f561064ede62b6063f623c2a6b3cadfd
stefaan.ternier <stefaan.ternier@gmail.com>
2012-04-27 18:49:39 +0200

    add mapAvailable attribute to Game bean

--- ARLearnBeans/src/org/celstec/arlearn2/beans/game/Config.java
+++ ARLearnBeans/src/org/celstec/arlearn2/beans/game/Config.java
@@ -1,13 +1,19 @@
+
 package org.celstec.arlearn2.beans.game;
 
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.celstec.arlearn2.beans.Bean;
+import org.celstec.arlearn2.beans.generalItem.GeneralItem;
 
 public class Config extends Bean {
 
 	
 	private Boolean scoring;
+	private Boolean mapAvailable;
+	public static String manualItemsType = "org.celstec.arlearn2.beans.generalItem.GeneralItem";
+	private List<GeneralItem> manualItems = new ArrayList<GeneralItem>();
 
 	public Boolean getScoring() {
 		return scoring;
@@ -16,6 +22,21 @@ public class Config extends Bean {
 	public void setScoring(Boolean scoring) {
 		this.scoring = scoring;
 	}
-	
+
+	public Boolean getMapAvailable() {
+		return mapAvailable;
+	}
+
+	public void setMapAvailable(Boolean mapAvailable) {
+		this.mapAvailable = mapAvailable;
+	}
+
+	public List<GeneralItem> getManualItems() {
+		return manualItems;
+	}
+
+	public void setManualItems(List<GeneralItem> manualItems) {
+		this.manualItems = manualItems;
+	}
 	
 }
