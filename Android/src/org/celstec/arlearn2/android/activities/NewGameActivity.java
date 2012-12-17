e67b360b5aee6fe282534154ef599b33a6daa38d
stefaan.ternier <stefaan.ternier@gmail.com>
2012-12-17 21:38:43 +0100

    adjust NewGameActivity to use methods of GameDelegator

--- Android/src/org/celstec/arlearn2/android/activities/NewGameActivity.java
+++ Android/src/org/celstec/arlearn2/android/activities/NewGameActivity.java
@@ -4,15 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.celstec.arlearn2.android.R;
-import org.celstec.arlearn2.android.db.PropertiesAdapter;
-import org.celstec.arlearn2.beans.game.Config;
+import org.celstec.arlearn2.android.delegators.GameDelegator;
 import org.celstec.arlearn2.beans.game.Game;
-import org.celstec.arlearn2.client.GameClient;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -21,8 +18,6 @@ import android.widget.EditText;
 
 public class NewGameActivity extends GeneralActivity {
 	
-	private String CLASSNAME = this.getClass().getName();
-
 	Game[] existingGames = null;
 	List<String> lGameTitles;
 
@@ -48,68 +43,36 @@ public class NewGameActivity extends GeneralActivity {
 		String sNewGameName = ((EditText) findViewById(R.id.etNewGame)).getText() + "";
 		String sGameAuthor = ((EditText) findViewById(R.id.etAuthor)).getText() + "";
 		Boolean bWithMap = ((CheckBox) findViewById(R.id.cbWithMap)).isChecked();
-		storeNewGame(sNewGameName, sGameAuthor, bWithMap);
-
+		if (checkFields(sNewGameName, sGameAuthor) && checkGameSameTitle(sNewGameName)) {
+			GameDelegator.getInstance().createGame(this, sNewGameName, sGameAuthor, bWithMap);
+		}
 	}
-
-	private void storeNewGame(String gamename, String gameauthor, Boolean withmap) {
-
-
-		if ((gamename.length() > 0) && (gameauthor.length() > 0)) {
-			if (!lGameTitles.contains(gamename)) {
-
-				Log.d(CLASSNAME, "Insert into DB new game.");
-				Game newGame = new Game();
-				newGame.setCreator(gameauthor);
-				newGame.setTitle(gamename);
-
-				Config newConfig = new Config();
-				newConfig.setMapAvailable(withmap);
-				newGame.setConfig(newConfig);
-				if (!insertGame(newGame)) {
-					Log.e(CLASSNAME, "Could not insert Game into DB");
-				}
-
-				NewGameActivity.this.finish();
-
-			} else {
-				Log.e(CLASSNAME, "There is already an existing game with this title");
-				AlertDialog.Builder builder = new AlertDialog.Builder(this);
-				builder.setMessage("There is already an existing game with this title").setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-						// NewGameActivity.this.finish();
-					}
-				});
-
-				builder.create();
-			}
-
-		} else {
-			Log.e(CLASSNAME, "Give alert in order to fill in the fields.");
+	
+	private boolean checkFields(String gamename, String gameauthor) {
+		if ((gamename.length() == 0) || (gameauthor.length() == 0)) {
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setMessage("Name and author cannot be empty fields.").setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int id) {
 					// NewGameActivity.this.finish();
 				}
 			});
+			builder.create();
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean checkGameSameTitle(String gameName) {
+		if (lGameTitles.contains(gameName)) {
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage("There is already an existing game with this title").setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					// NewGameActivity.this.finish();
+				}
+			});
 
 			builder.create();
-		}
-
-	}
-
-	private boolean insertGame(Game theGame) {
-
-		Game g = new Game();
-		PropertiesAdapter pa = new PropertiesAdapter(this);
-
-		g = GameClient.getGameClient().createGame(pa.getFusionAuthToken(), theGame);
-		if (g.getErrorCode() != null) {
-			if (g.getErrorCode() == 0) { //TODO reactivate the following code
-//			if (g.getErrorCode() == GameClient.ERROR_DESERIALIZING) {
-				Log.e(CLASSNAME, "Exception deserializing game " + theGame.getTitle() + ".");
-				return false;
-			}
+			return false;
 		}
 		return true;
 	}
